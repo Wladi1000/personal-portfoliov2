9bb4bf5b61b180cb7f55c728d50311847bc4f8f3
Wladi1000 <62582756+Wladi1000@users.noreply.github.com>
2024-03-01 17:48:33 -0400

    Update experience.vue

--- src/components/widgets/1x1/experience.vue
+++ src/components/widgets/1x1/experience.vue
@@ -11,7 +11,7 @@ const { spanish } = toRefs(props);
 <template>
   <div class="home__container small" style="background-color: #0385ffbc">
     <div style="display: grid; place-items: center" v-show="spanish">
-      <h1>+2</h1>
+      <h1>+3</h1>
       <p>AÑOS DE EXPERIENCIA</p>
     </div>
     <div style="display: grid; place-items: center" v-show="!spanish">
